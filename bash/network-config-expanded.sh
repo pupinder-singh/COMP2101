970bfc3edf4b2205239716d275af448fde149e2b
pupinder-singh <59669520+pupinder-singh@users.noreply.github.com>
2020-07-07 19:31:31 -0400

    lab 4

--- bash/network-config-expanded.sh
+++ bash/network-config-expanded.sh
@@ -61,35 +61,27 @@ EOF
 # Per-interface report
 #####
 # define the interface being summarized
-#interface="eno1"
+interface="eno1"
 
 # Find an address and hostname for the interface being summarized
 # we are assuming there is only one IPV4 address assigned to this interface
+ipv4_address=$(ip a s $interface|awk -F '[/ ]+' '/inet /{print $3}')
+ipv4_hostname=$(getent hosts $ipv4_address | awk '{print $2}')
 
 # Identify the network number for this interface and its name if it has one
-interface=$(lshw -class network | awk '/logical name:/{print $3}')
-declare -a inter=($interface)
-# A loop generate for processing each work in the array
-for i in "${inter[@]}"; do
-# For bypass loopback interface, use conditions
-    if [[ $i = lo* ]] ;
-		then continue ;
-	fi
- #For assigning command's output to the variable, use simple lines
- ipv4_address=$(ip a s $i | awk -F '[/ ]+' '/inet /{print $3}')
- ipv4_hostname=$(getent hosts $ipv4_address | awk '{print $2}')
- network_address=$(ip route list dev $i scope link|cut -d ' ' -f 1)
- network_number=$(cut -d / -f 1 <<<"$network_address")
- network_name=$(getent networks $network_number|awk '{print $1}')
- #put a command to print
- echo Interface $i:
- echo ===============
- echo Address         : $ipv4_address
- echo Name            : $ipv4_hostname
- echo Network Address : $network_address
- echo Network Name    : $network_name
-#Now end this loop
-done
+network_address=$(ip route list dev $interface scope link|cut -d ' ' -f 1)
+network_number=$(cut -d / -f 1 <<<"$network_address")
+network_name=$(getent networks $network_number|awk '{print $1}')
+
+cat <<EOF
+Interface $interface:
+===============
+Address         : $ipv4_address
+Name            : $ipv4_hostname
+Network Address : $network_address
+Network Name    : $network_name
+
+EOF
 #####
 # End of per-interface report
 #####
